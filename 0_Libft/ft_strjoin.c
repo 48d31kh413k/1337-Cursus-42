2cfb11dcaf1ee8409d4ada73508570fb210c326b
48d31kh413k <65833007+48d31kh413k@users.noreply.github.com>
2021-09-22 12:00:57 +0100

    Add additional functions

---
+++ 0_Libft/ft_strjoin.c
@@ -0,0 +1,28 @@
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*new;
+	int		i;
+	int		len;
+
+	i = 0;
+	if (!s1 || !s2)
+		return (NULL);
+	len = ft_strlen(s1) + ft_strlen(s2);
+	new = (char *)malloc(sizeof(char) * len + 1);
+	if (!new)
+		return (NULL);
+	while (s1[i])
+	{
+		new[i] = s1[i];
+		i++;
+	}
+	while (s2[i])
+	{
+		new[i] = s2[i];
+		i++;
+	}
+	new[i] = '\0';
+	return (new);
+}
