dce3b757d383b46e4bd84a07abb2a1fd4f4271eb
48d31kh413k <65833007+48d31kh413k@users.noreply.github.com>
2021-09-22 21:45:56 +0100

    Update libft.h

--- 0_Libft/libft.h
+++ 0_Libft/libft.h
@@ -4,6 +4,11 @@
 # include <stdlib.h>
 # include <unistd.h>
 
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}			t_list;
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
 int		ft_isalnum(int c);
