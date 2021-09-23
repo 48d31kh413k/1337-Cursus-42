103c95736c92284545abbaf2b51e43381b659e10
48d31kh413k <65833007+48d31kh413k@users.noreply.github.com>
2021-09-23 20:00:28 +0100

    Add Bonus part

---
+++ 0_Libft/ft_lstnew_bonus.c
@@ -0,0 +1,13 @@
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*new_node;
+
+	new_node = (t_list *)malloc(sizeof(t_list));
+	if (!new_node)
+		return (NULL);
+	new_node->content = content;
+	new_node->next = NULL;
+	return (new_node);
+}
