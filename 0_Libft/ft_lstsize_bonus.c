39932865cd037434b041c3dbe90314575c04f451
48d31kh413k <65833007+48d31kh413k@users.noreply.github.com>
2021-09-23 20:17:55 +0100

    Add Bonus part

---
+++ 0_Libft/ft_lstsize_bonus.c
@@ -0,0 +1,16 @@
+#include "libft.h"
+
+int	ft_lstsize(t_list *lst)
+{
+	int		count;
+	t_list	*node;
+
+	node = lst;
+	count = 0;
+	while (node != NULL)
+	{
+		count++;
+		node = node->next;
+	}
+	return (count);
+}
