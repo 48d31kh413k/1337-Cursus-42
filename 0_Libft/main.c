6dc3e29434248e4174d929e95ada0500e5bdbf33
48d31kh413k <65833007+48d31kh413k@users.noreply.github.com>
2021-09-25 20:40:12 +0100

    Update Libft tester

--- 0_Libft/main.c
+++ 0_Libft/main.c
@@ -105,6 +105,75 @@ static char	isalpha_test(int flag, int c)
 	}
 }
 
+static char	isascii_test(int flag, int c)
+{
+	char	OK;
+	char	NG;
+
+	OK = 'Y';
+	NG = 'N';
+	if (flag == TRUE)
+	{
+		if (ft_isascii(c))
+			return (OK);
+		else
+			return (NG);
+	}
+	else
+	{
+		if (!ft_isascii(c))
+			return (OK);
+		else
+			return (NG);
+	}
+}
+
+static char	isdigit_test(int flag, int c)
+{
+	char	OK;
+	char	NG;
+
+	OK = 'Y';
+	NG = 'N';
+	if (flag == TRUE)
+	{
+		if (ft_isdigit(c))
+			return (OK);
+		else
+			return (NG);
+	}
+	else
+	{
+		if (!ft_isdigit(c))
+			return (OK);
+		else
+			return (NG);
+	}
+}
+
+static char	isprint_test(int flag, int c)
+{
+	char	OK;
+	char	NG;
+
+	OK = 'Y';
+	NG = 'N';
+	if (flag == TRUE)
+	{
+		if (ft_isprint(c))
+			return (OK);
+		else
+			return (NG);
+	}
+	else
+	{
+		if (!ft_isprint(c))
+			return (OK);
+		else
+			return (NG);
+	}
+}
+
 int main()
 {
 	char	*test_atoi;
@@ -112,6 +181,9 @@ int main()
 	char	*test_calloc;
 	char	*alnum_test;
 	char	*test_isalpha;
+	char	*test_isascii;
+	char	*test_isdigit;
+	char	*test_isprint;
 	int	i;
 
 	test_atoi = (char *)malloc(sizeof(char) * 19);
@@ -119,6 +191,9 @@ int main()
 	alnum_test = (char *)malloc(sizeof(char) * 10);
 	test_calloc = (char *)malloc(sizeof(char) * 4);
 	test_isalpha = (char *)malloc(sizeof(char) * 6);
+	test_isascii = (char *)malloc(sizeof(char) * 8);
+	test_isdigit = (char *)malloc(sizeof(char) * 6);
+	test_isprint = (char *)malloc(sizeof(char) * 11);
 	test_atoi[0] = atoi_test("42");
 	test_atoi[1] = atoi_test("1289439");
 	test_atoi[2] = atoi_test("-66847");
@@ -163,11 +238,38 @@ int main()
 	test_isalpha[3] = isalpha_test(FALSE, '!');
 	test_isalpha[4] = isalpha_test(FALSE, '9');
 	test_isalpha[5] = '\0';
+	test_isascii[0] = isascii_test(TRUE, 'a');
+	test_isascii[1] = isascii_test(TRUE, 'Y');
+	test_isascii[2] = isascii_test(TRUE, 'Z');
+	test_isascii[3] = isascii_test(TRUE, '!');
+	test_isascii[4] = isascii_test(TRUE, '9');
+	test_isascii[5] = isascii_test(FALSE, -1);
+	test_isascii[6] = isascii_test(FALSE, 128);
+	test_isascii[7] = '\0';
+	test_isdigit[0] = isdigit_test(TRUE, '1');
+	test_isdigit[1] = isdigit_test(TRUE, '0');
+	test_isdigit[2] = isdigit_test(TRUE, '7');
+	test_isdigit[3] = isdigit_test(FALSE, '#');
+	test_isdigit[4] = isdigit_test(FALSE, 'a');
+	test_isdigit[5] = '\0';
+	test_isprint[0] = isprint_test(TRUE, '1');
+	test_isprint[1] = isprint_test(TRUE, '0');
+	test_isprint[2] = isprint_test(TRUE, '7');
+	test_isprint[3] = isprint_test(TRUE, '#');
+	test_isprint[4] = isprint_test(TRUE, 'a');
+	test_isprint[5] = isprint_test(TRUE, 'T');
+	test_isprint[6] = isprint_test(FALSE, 127);
+	test_isprint[7] = isprint_test(FALSE, 23);
+	test_isprint[8] = isprint_test(FALSE, 0);
+	test_isprint[9] = isprint_test(FALSE, 128);
+	test_isprint[10] = '\0';
+	printf("\n********** LIBFT TESTER **********\n\n");
 	printf(" --------- \n");
 	printf("| ft_atoi |\n");
 	printf(" --------- \n");
 	for(i = 0; i < 18; ++i)
 		printf("test [%d] = %c \n", i, test_atoi[i]);
+	printf("\n");	
 	/*printf("ft_bzero \n");
 	for(i = 0; i < 3; ++i)
 		printf("test [%d] = %c \n", i , test_bzero[i]); */
@@ -176,15 +278,36 @@ int main()
 	printf(" ----------- \n");
 	for(i = 0; i < 3; ++i)
 		printf("test [%d] = %c \n", i , test_calloc[i]);
+	printf("\n");
 	printf(" ------------ \n");
 	printf("| ft_isalnum |\n");
 	printf(" ------------ \n");
 	for( i = 0; i < 10; ++i)
 		printf("test [%d] = %c \n", i , alnum_test[i]);
+	printf("\n");
 	printf(" ------------ \n");
 	printf("| ft_islapha |\n");
 	printf(" ------------\n");
 	for( i = 0; i < 5; ++i)
 		printf("test [%d] = %c \n", i , test_isalpha[i]);
+	printf("\n");
+	printf(" ------------ \n");
+	printf("| ft_isascii |\n");
+	printf(" ------------ \n");
+	for(i = 0; i < 7; ++i)
+		printf("test [%d] = %c \n", i, test_isascii[i]);
+	printf("\n");
+	printf(" ------------ \n");
+	printf("| ft_isdigit |\n");
+	printf(" ------------ \n");
+	for(i = 0; i < 5; ++i)
+		printf("test [%d] = %c \n", i, test_isdigit[i]);
+	printf("\n");
+	printf(" ------------ \n");
+	printf("| ft_isprint |\n");
+	printf(" ------------ \n");
+	for(i = 0; i < 9; ++i)
+		printf("test [%d] = %c \n", i, test_isprint[i]);
+	
 	return (0);
 }
