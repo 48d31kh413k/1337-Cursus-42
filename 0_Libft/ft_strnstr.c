d28136bb9c2d8f7e512c9704be1a0b5968f429cb
48d31kh413k <65833007+48d31kh413k@users.noreply.github.com>
2021-10-04 19:31:55 +0100

    Update ft_strnstr.c

--- 0_Libft/ft_strnstr.c
+++ 0_Libft/ft_strnstr.c
@@ -1,6 +1,6 @@
 #include "libft.h"
 
-char	*ft_strstr(const char *str, const char *to_find, size_t len)
+char	*ft_strnstr(const char *str, const char *to_find, size_t len)
 {
 	size_t	i;
 	size_t	j;
