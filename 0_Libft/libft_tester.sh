2d4bf33373fecac00d7ad1fd2ea475e3c186927b
48d31kh413k <65833007+48d31kh413k@users.noreply.github.com>
2021-10-08 17:43:06 +0100

    Update

--- 0_Libft/libft_tester.sh
+++ 0_Libft/libft_tester.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
-make all
 make bonus
 make clean
 gcc -Wall -Wextra -Werror main.c libft.a
+./a.out
+gcc -Wall -Wextra -Werror main_fd.c libft.a
+./a.out
+gcc -Wall -Wextra -Werror main_bonus.c libft.a
 ./a.out
 make fclean
 rm a.out
