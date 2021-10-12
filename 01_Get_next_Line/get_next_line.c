848fef93dfb1f7507f542aa8b81efb48d059ab4f
48d31kh413k <65833007+48d31kh413k@users.noreply.github.com>
2021-10-12 10:41:24 +0100

    Update

--- 01_Get_next_Line/get_next_line.c
+++ 01_Get_next_Line/get_next_line.c
@@ -63,33 +63,3 @@ char	*get_next_line(int fd)
 	next_line = ft_get_next_line(line);
 	return (next_line);
 }
-
-int	main(void)
-{
-	char	*line;
-	int		i;
-	int		fd1;
-	int		fd2;
-	int		fd3;
-	fd1 = open("tests/test.txt", O_RDONLY);
-	fd2 = open("tests/test2.txt", O_RDONLY);
-	fd3 = open("tests/test3.txt", O_RDONLY);
-	i = 1;
-	while (i < 7)
-	{
-		line = get_next_line(fd1);
-		printf("line [%02d]: %s", i, line);
-		free(line);
-		line = get_next_line(fd2);
-		printf("line [%02d]: %s", i, line);
-		free(line);
-		line = get_next_line(fd3);
-		printf("line [%02d]: %s", i, line);
-		free(line);
-		i++;
-	}
-	close(fd1);
-	close(fd2);
-	close(fd3);
-	return (0);
-}
